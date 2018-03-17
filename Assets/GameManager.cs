321ea380feb383c6f7ff2c48787ad7560cd98cc5
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2018-03-18 04:26:26 +0900

    完了： ゲームマネージャーを実装。 未完了： エリア拡張

---
+++ Assets/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // 制限時間
+    [SerializeField]
+    float timeLimit;
+    float time;
+    bool isStartPandemic;
+
+    // Use this for initialization
+    void Start () {
+        time = 0.0f;
+        isStartPandemic = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 感染後の処理 //
+        if (isStartPandemic == false) return;      
+        time += Time.deltaTime;
+        Debug.Log("TimeLimit : " + time.ToString("F") + "s / " + timeLimit.ToString("F") + "s");
+        if (timeLimit < time) FinishGame();
+
+       // エリア内の市民を全員感染させた時の処理 //
+       // if(全員感染) ステージを拡張する
+
+	}
+
+    // publicメソッド //
+    public void StartGame()
+    {
+        isStartPandemic = true;
+    }
+
+    // privateメソッド //
+    void FinishGame()
+    {
+        Debug.Log("FinishedGame");
+        Debug.Break();
+    }
+}
