fd4186355b72424cbb809031b1f1289c1a1499ee
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2018-03-19 14:57:14 +0900

    完了： ウィルスクラスのフレーム 次： クラス内部の実装

---
+++ Assets/Script/Virus/Skill.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// スキル
+public abstract class Skill
+{
+    VirusAbility.BounsStatus bouns;
+
+    // スキルの種類の取得
+    public abstract string GetType();
+
+    // 更新
+    public abstract void Update();
+
+    public VirusAbility.BounsStatus bounsStatus
+    {
+        get { return bouns; }
+    }
+}
+
+// ステータス増加のみのスキル
+//memo それ以外のユニークスキルは他のスクリプト 
+public class StatusBaffler : Skill
+{
+    public override string GetType()
+    {
+        return "Bouns";
+    }
+
+    public override void Update()
+    {
+        
+    }
+}
+
+
---
+++ Assets/VirusAbility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusAbility : MonoBehaviour {
+
+    // ボーナスステータス(％)
+    public class BounsStatus
+    {
+        float moveSpeedRate;
+    }
+
+    BounsStatus bounsStatus;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    // プロパティ
+    public BounsStatus bouns
+    {
+        get { return bounsStatus; }
+    }
+
+}
