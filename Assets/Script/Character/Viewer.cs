44d830063a39b12f8fef03cd8cf2d9c2b06da81f
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2018-03-21 00:56:16 +0900

    完了： ファイルパスの変更。 簡易的な視野の実装。

---
+++ Assets/Script/Character/Viewer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer : MonoBehaviour {
+
+    [SerializeField]
+    float range = 1.0f;
+
+    string targetTag;
+    GameObject target;
+    float minLength = float.MaxValue;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SphereCollider>().radius = range;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+            minLength = (transform.position - target.transform.position).magnitude;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag != tag) return;
+        float length = (transform.position - other.gameObject.transform.position).magnitude;
+        if (minLength > length)
+        {
+            target = other.gameObject;
+            minLength = length;
+        } 
+    }
+
+    public void Target(string tag)
+    {
+        targetTag = tag;
+    }
+
+    public GameObject GetClose()
+    {
+        return target;
+    }
+
+    //public GameObject GetClose(ViewType type)
+    //{
+    //    Vector3 position = transform.position; 
+
+    //    // オブジェクトが存在しない
+    //    if (actors.Count == 0) return null;
+
+    //    // 最短のオブジェクトを求める
+    //    GameObject closeActor = actors[0];
+    //    float closeLength = (closeActor.transform.position - position).magnitude;
+    //    int count = actors.Count;
+    //    for (int i = 0; i < count; i++)
+    //    {
+    //        float length = (actors[i].transform.position - position).magnitude;
+    //        if (closeLength > length)
+    //        {
+    //            closeActor = actors[i];
+    //            closeLength = length;
+    //        }                    
+    //    }
+        
+    //    return closeActor;
+    //}
+}
