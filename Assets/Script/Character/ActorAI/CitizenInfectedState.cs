80825f4bf0574c9a984674deacdc724685114a6b
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2018-04-06 07:58:33 +0900

    偽フロッキング

--- Assets/Script/Character/ActorAI/CitizenInfectedState.cs
+++ Assets/Script/Character/ActorAI/CitizenInfectedState.cs
@@ -4,39 +4,63 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class CitizenInfectedState : CitizenAI.State {
- 
-    //void CitizenNormalState(StateData data)
-    //{
-        
-
-    //}
+  
     [SerializeField]
     static private float angle;
     private bool wasInitialize = false;
+
+    Vector3 hogePlayer;
+
     public override void Excute(StateData data)
     {
-        // 
-        if (wasInitialize)
+        wasInitialize = true;
+        if (Input.GetKey(KeyCode.R)) wasInitialize = false;
+        if (wasInitialize == false)
         {
+            // ナビゲーション対象のエージェント
             NavMeshAgent agent = data.ai.GetComponent<NavMeshAgent>();
+            
+            // 拡張範囲の取得
             ExpansionControl expansion = data.virus.GetOriginal().GetComponent<ExpansionControl>();
-            GameObject[] infectedPerson = GameObject.FindGameObjectsWithTag("InfectionArea");
-            Vector3 vec;
-            Vector3 avevec;
+
+            //GameObject[] infectedPerson = GameObject.FindGameObjectsWithTag("InfectionArea");
+
+            //Vector3 vec;
+            //Vector3 avevec;
+            
             // 感染原(プレイヤ)
-            Virus target = data.virus.GetOriginal();
-            // 感染者のウィルス
+            Virus target = data.virus.GetOriginal();            
             Virus selfVirus = data.virus;
-            if (target.gameObject == null) return;
 
-            vec = selfVirus.gameObject.transform.position - target.gameObject.transform.position;
-            vec.Normalize();
+            //!　ゲームの仕様上　ターゲットが見つからないことはない
+            if (target.gameObject == null) Debug.Break();
 
-            angle += 360.0f / infectedPerson.Length;
+            //vec = selfVirus.gameObject.transform.position - target.gameObject.transform.position;
+            //vec.Normalize();
 
-            avevec = Quaternion.Euler(0.0f, angle, 0.0f) * vec;
+            //angle += 360.0f / infectedPerson.Length;
 
-            agent.destination = avevec * expansion.ExpansionArea;
+            //avevec = Quaternion.Euler(0.0f, angle, 0.0f) * vec;
+
+            //agent.destination = avevec * expansion.ExpansionArea;
+
+            Vector3 playerVec = target.transform.position - hogePlayer;
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            Vector3 vec = new Vector3(x, 0, z);
+            playerVec = vec.normalized * 5.0f;
+            hogePlayer = target.transform.position;
+
+            // 拡張範囲を考慮した差
+            Vector3 offset = selfVirus.gameObject.transform.position - target.transform.position;
+            //offset = playerVec;
+            offset.Normalize();
+            offset *= expansion.ExpansionArea;
+            offset += playerVec;
+
+            // 目標地点
+            Vector3 targetPos = target.gameObject.transform.position + offset;
+            agent.SetDestination(targetPos);
         }  
         
     }
