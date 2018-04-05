8bd42ad4aac736e62f4ad3f2aa736d20780b1bce
watabe <takumi.watabe.program@gmail.com>
2018-04-05 10:46:43 +0900

    UIコントロール実装

---
+++ Assets/Script/LuxusDebug.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuxusDebug : MonoBehaviour
+{
+    private GameObject m_player;
+    [SerializeField]
+    private bool m_view = true;
+    [SerializeField]
+    private GameObject m_luxusDebugObj;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 感染者を探す
+        m_player = GameObject.FindGameObjectWithTag("Player");
+
+        if (m_view) ViewDebug(m_player);
+
+    }
+
+    private void ViewDebug(GameObject obj)
+    {
+        // 新しい感染者にデバッグ表示
+        GameObject debugObj = Instantiate(m_luxusDebugObj);
+        debugObj.transform.parent = obj.transform;
+        debugObj.transform.localPosition = (obj.transform.localRotation) * -debugObj.transform.position;
+        m_view = false;
+    }
+
+    //private void DeleteDebug(GameObject[] infectedPerson)
+    //{
+    //    foreach (GameObject obj in infectedPerson)
+    //    {
+    //        Transform thisObj = obj.transform.Find(m_areaDebugObj.name + "(Clone)");
+    //        if (thisObj) Destroy(thisObj.gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Script/UIControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    public float size;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeSize(size);
+
+    }
+
+    public void ChangeSize(float num)
+    {
+        transform.localScale = new Vector3(num, 0.1f, num);
+    }
+}
