6a5841e3f676e9a8e2cd86b4f4f11f5d6b2126a2
yuya <yuya.okada.program@gmail.com>
2018-04-07 02:13:25 +0900

    Rankingに必要なものの追加

---
+++ Assets/Script/Network/SaveStr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveStr : MonoBehaviour
+{
+    [SerializeField]
+    private string userName;
+    [SerializeField]
+    private float resultScore;
+
+    // Use this for initialization
+    void Start()
+    {
+        userName = "";
+        resultScore = 0;
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetuserName(string name)
+    {
+        userName = name;
+    }
+    public void SetresultScore(float score)
+    {
+        resultScore = score;
+    }
+
+    public string GetuserName()
+    {
+        return userName;
+    }
+
+    public float GetresultScore()
+    {
+        return resultScore;
+    }
+
+}
