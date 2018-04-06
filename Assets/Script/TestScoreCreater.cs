ec46332ca729904ebfe6659b71fa4d07b89cde0e
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2018-04-07 01:54:02 +0900

    ランキング以降の部分の切り替えテスト用シーンの作成 オブジェクトとスクリプトの作成のみ

---
+++ Assets/Script/TestScoreCreater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class TestScoreCreater : MonoBehaviour {
+
+    [SerializeField]
+    string nextScene;
+
+    [SerializeField]
+    float score;
+
+    [SerializeField]
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+    }
+}
