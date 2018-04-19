9edc5d128f9b443fc15c89b2cf57dc6d8b1ead45
yuya <yuya.okada.program@gmail.com>
2018-04-19 17:06:54 +0900

    comboの修正

---
+++ Assets/Script/ComboScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboScript : MonoBehaviour
+{
+    private static int comboNum;
+
+    [SerializeField]
+    private float comboTime = 3.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        comboNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	}
+
+    public int PlusCombo()
+    {
+        return comboNum++;
+    }
+
+    public int GetCombo()
+    {
+        return comboNum;
+    }
+
+    public IEnumerator ComboCoroutine()
+    {
+        yield return new WaitForSeconds(comboTime);
+        comboNum = 0;
+    }
+}
