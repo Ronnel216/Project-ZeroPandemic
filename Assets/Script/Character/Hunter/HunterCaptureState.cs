455d70824c638c0f053039b7ea4c23dfaa0c8461
sawada <s162093>
2018-04-28 20:47:04 +0900

    ・ハンター作成(途中)

---
+++ Assets/Script/Character/Hunter/HunterCaptureState.cs
@@ -0,0 +1,34 @@
+﻿//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+//! @file   HunterCaptureState.cs
+//!
+//! @brief  捕獲状態
+//!
+//! @date   日付　2018/04/28
+//!
+//! @author 制作者名 澤田
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterCaptureState : HunterController.HunterState {
+
+    int a = 60;             // 仮
+    public override void Enter(HunterController hunter)
+    {
+
+    }
+
+    public override void Update(HunterController hunter)
+    {
+        // 仮 ================================================
+        a--;
+        Debug.Log("捕獲中：あと "+a.ToString());
+        if (a <= 0)
+        {
+            hunter.ChangeState(new HunterCooldownState());
+        }   
+        // ====================================================
+    }
+}
---
+++ Assets/Script/Character/Hunter/HunterCooldownState.cs
@@ -0,0 +1,32 @@
+﻿//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+//! @file   HunterCooldownState.cs
+//!
+//! @brief  クールダウン状態
+//!
+//! @date   日付　2018/04/28
+//!
+//! @author 制作者名 澤田
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterCooldownState : HunterController.HunterState
+{
+    private float m_time = 0.0f;               // 時間
+
+    public override void Enter(HunterController hunter)
+    {
+        
+    }
+
+    public override void Update(HunterController hunter)
+    {
+        m_time += Time.deltaTime;
+
+        // クールダウン終了
+        if (m_time >= hunter.CoolTime)
+            hunter.ChangeState(new HunterChaseState());
+    }
+}
