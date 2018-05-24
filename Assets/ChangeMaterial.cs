a818d0b13e18df2ffa31c4a8b6700ecf3d8c5d11
watabe <takumi.watabe.program@gmail.com>
2018-05-24 16:15:54 +0900

    各キャラクターの実装 アニメーション実装 感染時にマテリアル変更でゾンビに 市民追加

---
+++ Assets/ChangeMaterial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ChangeMaterial : MonoBehaviour {
+
+    [SerializeField]
+    Material mat;
+
+    bool changeFlag;
+    GameObject parent;
+    // Use this for initialization
+    void Start () {
+        parent = gameObject.transform.parent.parent.gameObject;
+        changeFlag = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(parent.tag== "InfectedActor")
+        {
+            if(changeFlag==false)
+            {
+                Debug.Log("111111");
+                ChangeMat();
+                changeFlag = true;
+            }
+        }
+	}
+    void ChangeMat()
+    {
+        SkinnedMeshRenderer smr = GetComponent<SkinnedMeshRenderer>();
+        Material[] mats = smr.materials;
+        for (int i = 0; i < mats.Length; i++)
+        {
+            mats[i] = mat;
+        }
+        smr.materials = mats;
+    }
+}
